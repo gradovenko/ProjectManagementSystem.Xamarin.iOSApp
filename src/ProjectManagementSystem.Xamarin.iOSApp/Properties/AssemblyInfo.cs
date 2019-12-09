83bead68b8ff28f1fca2446b5aed20e94b5bd61c
gradovenko <grad1242@gmail.com>
2019-12-10 01:17:58 +0500

    Some upgrade ios app xamarin project

--- src/ProjectManagementSystem.Xamarin.iOSApp/Properties/AssemblyInfo.cs
+++ src/ProjectManagementSystem.Xamarin.iOSApp/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ProjectManagementSystem.Xamarin.iOSApp")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("F5335F45-800F-4F69-8785-3B800DC864D4")]
+[assembly: Guid("50c7b8c9-e664-45af-b88e-0c9b8b9c1be1")]
 
 // Version information for an assembly consists of the following four values:
 //
